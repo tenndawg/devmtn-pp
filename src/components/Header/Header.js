2b29c2c7254dd8c40bc5b3c58b0c02f09d9a8a75
tenndawg <nickolas.tenney@gmail.com>
2018-03-06 14:46:13 -0700

    removed bad code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
-import {Helmet} from 'react-helmet';
 import "./header.css";
 
 export default class Header extends Component {  
     render() {
-        return(
-            <div>
-            <div class="topnav" id="myTopnav">
-                <a href="#home" class="active">Home</a>
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-                <a href="javascript:void(0)" class="icon" onClick="myFunction()">&#9776;</a>
-            </div>
+        return(        
             <div className="menu">
                 <div className="siteName">
                     <h1>TennDawg's Den</h1>
@@ -29,7 +20,6 @@ export default class Header extends Component {
                     <img src={require("../../img/facebookicon.png")} alt="facebook"/>
                     <img src={require("../../img/tumblricon.png")} alt="tumblr"/>
                 </div>
-            </div>
             </div>
         )
     }
