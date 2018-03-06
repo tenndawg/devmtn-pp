8b50226c331645e46545bdd0ab64d3e3e129b39f
tenndawg <nickolas.tenney@gmail.com>
2018-03-06 14:33:11 -0700

    making nav responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import {Helmet} from 'react-helmet';
 import "./header.css";
 
-export default class Header extends Component {
+export default class Header extends Component {  
     render() {
         return(
+            <div>
+            <div class="topnav" id="myTopnav">
+                <a href="#home" class="active">Home</a>
+                <a href="#news">News</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+                <a href="javascript:void(0)" class="icon" onClick="myFunction()">&#9776;</a>
+            </div>
             <div className="menu">
                 <div className="siteName">
                     <h1>TennDawg's Den</h1>
@@ -15,10 +24,12 @@ export default class Header extends Component {
                     <h2>Menu 4</h2>
                 </div>
                 <div className="icons">
-                    <img src="../../img/searchicon.png" alt="search"/>
-                    <img src="../../img/twittericon.png" alt="twitter"/>
-                    <img src="../../img/facebookicon.png" alt="facebook"/>
+                    <img src={require("../../img/searchicon.png")} alt="search"/>
+                    <img src={require("../../img/twittericon.png")} alt="twitter"/>
+                    <img src={require("../../img/facebookicon.png")} alt="facebook"/>
+                    <img src={require("../../img/tumblricon.png")} alt="tumblr"/>
                 </div>
+            </div>
             </div>
         )
     }
