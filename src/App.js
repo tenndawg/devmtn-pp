d0f3d2ab7e229c8dbce894e0dadd5bfe23731eb2
tenndawg <nickolas.tenney@gmail.com>
2018-03-14 13:28:27 -0600

    Separated the components and working on the sports Header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import VGMain from './components/VGMain/VGMain';
+import SportsMain from './components/Sports/SportsMain/SportsMain';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         
           <Switch>
             <Route path='/vgmain' exact={true} component={ VGMain } />
+            <Route path='/sportsmain' exact={true} component={ SportsMain } />
           </Switch>
       
         </Router>
