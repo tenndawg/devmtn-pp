4b8f3a7453105456f51a6a86eee6310d4cdcfc9b
tenndawg <nickolas.tenney@gmail.com>
2018-03-26 11:51:33 -0600

    adding the sports section boxes

--- src/components/Sports/SportsMain/SportsMain.js
+++ src/components/Sports/SportsMain/SportsMain.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import Header2 from './Header2/Header2';
 import NewsTicker from './NewsTicker/NewsTicker';
+import Boxes from './Boxes/Boxes';
 
 export default class SportsMain extends Component {
     render() {
         return (
             <div>
                 <Header2/>
+                <Boxes/>
             </div>
         )
     }
