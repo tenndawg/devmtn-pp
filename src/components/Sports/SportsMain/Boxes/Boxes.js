911568e57c4db9773ad36ac7d85aae2906052db3
tenndawg <nickolas.tenney@gmail.com>
2018-03-26 12:24:18 -0600

    all backgrounds of the boxes section

---
+++ src/components/Sports/SportsMain/Boxes/Boxes.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import "./boxes.css";
+
+export default class Boxes extends Component {
+
+    render() {
+        return(
+            <div className="boxCom">
+                <div className="boxesMain">
+                    <div className="nfl">
+                        <img src={require("../../../../img/nflbg.jpg")}/>
+                        {/* <p>NFL</p> */}
+                    </div>
+                    <div className="quad1">
+                        <img src={require("../../../../img/nbabg.jpg")}/>
+                        <p>NBA</p>
+                    </div>
+                    <div className="quad2">
+                        <img src={require("../../../../img/nhlbg.jpg")}/>
+                        <p>NHL</p>
+                    </div>
+                    <div className="quad3">
+                        <img src={require("../../../../img/mlbbg.jpg")}/>
+                        <p>MLB</p>
+                    </div>
+                    <div className="quad4">
+                        <img src={require("../../../../img/mlsbg.jpg")}/>
+                        <p>MLS</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+
+}
