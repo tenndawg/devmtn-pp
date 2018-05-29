979bde7f73844b8e33dd14d145c0e2591b6eb661
tenndawg <nickolas.tenney@gmail.com>
2018-05-29 12:52:20 -0600

    fiddling with css

--- src/components/VideoGames/VGMain/Sections/Sections.js
+++ src/components/VideoGames/VGMain/Sections/Sections.js
@@ -7,16 +7,16 @@ export default class Sections extends Component {
             <div>
                 <div className="boxes">
                     <div className="container">
-                        <a href="localhost:3000/xbox"><img src={require("../../../../img/xbox.png")} alt="Xbox"/><p>Xbox</p></a>
+                        <a href="localhost:3000/#/xbox"><img src={require("../../../../img/xbox.png")} alt="Xbox"/><p>Xbox</p></a>
                     </div>
                     <div className="container">
-                        <a href="localhost:3000/ps"><img src={require("../../../../img/ps.png")} alt="Playstation"/><p>Playstation</p></a>
+                        <a href="localhost:3000/#/ps"><img src={require("../../../../img/ps.png")} alt="Playstation"/><p>Playstation</p></a>
                     </div>
                     <div className="container">
-                        <a href="localhost:3000/pc"><img src={require("../../../../img/pc.png")} alt="PC"/><p>PC</p></a>
+                        <a href="localhost:3000/#/pc"><img src={require("../../../../img/pc.png")} alt="PC"/><p>PC</p></a>
                     </div>
                     <div className="container">
-                        <a href="localhost:3000/nintendo"><img src={require("../../../../img/nintendo.png")} alt="Nintendo"/><p>Nintendo</p></a>
+                        <a href="localhost:3000/#/nintendo"><img src={require("../../../../img/nintendo.png")} alt="Nintendo"/><p>Nintendo</p></a>
                     </div>
                 </div>
             </div>
