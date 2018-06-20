e841a67360d68add86190dcfd77c2fa55168a155
tenndawg <nickolas.tenney@gmail.com>
2018-06-20 13:50:42 -0600

    editing xbox page css

--- src/components/Sports/SportsMain/Boxes/Boxes.js
+++ src/components/Sports/SportsMain/Boxes/Boxes.js
@@ -10,31 +10,36 @@ export default class Boxes extends Component {
                     <div className="nfl">
                         <a href={this.props.link1} target="_blank">
                             <img src={this.props.mainBlock}/>
-                            <p>{this.props.title1}</p>
+                            <p className="midP">{this.props.title1}</p>
+                            <span className="middle">{this.props.hover1}</span>
                         </a>
                     </div>
                     <div className="quad1">
                         <a href={this.props.link2} target="_blank">
                             <img src={this.props.quad1}/>
-                            <p>{this.props.title2}</p>
+                            <p className="midP">{this.props.title2}</p>
+                            <span className="middle">{this.props.hover2}</span>
                         </a>
                     </div>
                     <div className="quad2">
                         <a href={this.props.link3} target="_blank">
                             <img src={this.props.quad2}/>
-                            <p>{this.props.title3}</p>
+                            <p className="midP">{this.props.title3}</p>
+                            <span className="middle">{this.props.hover3}</span>
                         </a>
                     </div>
                     <div className="quad3">
                         <a href={this.props.link4} target="_blank">
                             <img src={this.props.quad3}/>
-                            <p>{this.props.title4}</p>
+                            <p className="midP">{this.props.title4}</p>
+                            <span className="middle">{this.props.hover4}</span>
                         </a>
                     </div>
                     <div className="quad4">
                         <a href={this.props.link5} target="_blank">
                             <img src={this.props.quad4}/>
-                            <p>{this.props.title5}</p>
+                            <p className="midP">{this.props.title5}</p>
+                            <span className="middle">{this.props.hover5}</span>
                         </a>
                     </div>
                 </div>
--- src/components/VideoGames/SubPages/Xbox.js
+++ src/components/VideoGames/SubPages/Xbox.js
@@ -52,11 +52,11 @@ export default class Xbox extends Component {
                     <img src={require("../../../img/xboxlogo.jpg")} alt="Video Game" className="pic"/>
                 </div>
                 <div className="boxStyle">
-                    <Boxes mainBlock={block1} title1={articles[0].source.name} link1={articles[0].url}
-                     quad1={block2} title2={articles[1].source.name} link2={articles[1].url}
-                     quad2={block3} title3={articles[2].source.name} link3={articles[2].url}
-                     quad3={block4} title4={articles[3].source.name} link4={articles[3].url}
-                     quad4={block5} title5={articles[4].source.name} link5={articles[4].url} />
+                    <Boxes mainBlock={block1} title1={articles[0].source.name} link1={articles[0].url} hover1={articles[0].title}
+                     quad1={block2} title2={articles[1].source.name} link2={articles[1].url} hover2={articles[1].title}
+                     quad2={block3} title3={articles[2].source.name} link3={articles[2].url} hover3={articles[2].title}
+                     quad3={block4} title4={articles[3].source.name} link4={articles[3].url} hover4={articles[3].title}
+                     quad4={block5} title5={articles[4].source.name} link5={articles[4].url} hover5={articles[4].title} />
                 </div>
                 <div className="newsReviews">
                     <AllNews />
